bcdc2e1234c1d97e631b51515d693e58760a2e3c
Manuel Alonso Bernabe <127040970+alonsobern@users.noreply.github.com>
2024-10-21 14:27:01 +0100

    Add files via upload

---
+++ interfaces/src/CocheCRUDImpl.java
@@ -0,0 +1,16 @@
+public class CocheCRUDImpl implements CocheCRUD{
+    @Override
+    public String save() {
+        return "Método Guardar";
+    }
+
+    @Override
+    public String findAll() {
+        return "Método Buscar Todos";
+    }
+
+    @Override
+    public String delete() {
+        return "Método Eliminar";
+    }
+}
